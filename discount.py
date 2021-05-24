2488785cda3cd490dc7b2778a6445c5866de3efa
Abhishek-1301 <84447693+Abhishek-1301@users.noreply.github.com>
2021-05-24 18:20:39 +0530

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,24 @@
+n=int(input('Enter total amount:'))
+print('Your total bill amount is:',n)
+if 1000<=n<2000:
+    d=0.1*n
+    print('Your discount is ',d)
+    print('Dear customer,your total bill payable is ',n-d)
+elif 2000<=n<3000:
+    d=0.2*n
+    print('Your discount is ',d)
+    print('Dear customer,your total bill payable is ',n-d)
+elif 3000<=n<5000:
+    d=0.3*n
+    print('Your discount is ',d)
+    print('Dear customer,your total bill payable is ',n-d)
+elif n<=5000:
+    d=0.4*n
+    print('Your discount is ',d)
+    print('Dear customer,your total bill payable is ',n-d)
+else n<1000:
+    print('Sorry!your total bill payable is ',n)
+
+
+    
+   
